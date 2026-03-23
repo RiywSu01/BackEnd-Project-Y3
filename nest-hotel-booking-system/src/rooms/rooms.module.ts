ca4f8f9243928ef2b30406c9bf40e61790e8eebd
RiywSu01 <riywsu01@gmail.com>
2026-03-23 18:35:13 +0700

    import AuthModule to the User and Rooms Module.ts

--- nest-hotel-booking-system/src/rooms/rooms.module.ts
+++ nest-hotel-booking-system/src/rooms/rooms.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { PrismaModule } from '../prisma/prisma.module'
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
