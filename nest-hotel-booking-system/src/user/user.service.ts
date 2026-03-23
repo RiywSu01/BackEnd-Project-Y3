4730d3c54490fb0b95b33a7e9edbcde6e6a2f396
RiywSu01 <riywsu01@gmail.com>
2026-03-23 17:22:19 +0700

    Updated Error handling in User Service

--- nest-hotel-booking-system/src/user/user.service.ts
+++ nest-hotel-booking-system/src/user/user.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import * as bcrypt from 'bcrypt';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class UserService {
@@ -13,63 +14,82 @@ export class UserService {
   
   //User Get Own Profile (For User to see their own profile)
   async GetProfile(username: string) {
-    const user = await this.prisma.users.findUnique( {where: {username}} );
-    if(!user){ //If user didnt exist
-      throw new NotFoundException(`User name: ${user}, not found.`);
+    try{
+      const user = await this.prisma.users.findUnique( {where: {username}} );
+      if(!user){ //If user didnt exist
+        throw new NotFoundException(`User name: ${user}, not found.`);
+      }
+      const {roles ,userPassword, ...Information} = user; // To extract the crucial information out.
+      return Information;
+    }catch(error){
+      throw new InternalServerErrorException("Something went wrong at GetProfile endpoint at UserService.");
     }
-    const {roles ,userPassword, ...Information} = user; // To extract the crucial information out.
-    return Information;
+
   }
 
   //User Update Own Profile
   async UpdateProfile(username: string, updateUserDto: UpdateUserDto) {
-    const user = await this.prisma.users.findUnique({where:{username}});
-    if(!user){
-      throw new BadRequestException("User not found.");
-    }
-
-    //Check if the username already have or not
-    if(updateUserDto.username === user.username){
-      throw new Error(`This username have been already used.`);
-    }
+    try{
+      const user = await this.prisma.users.findUnique({where:{username}});
+      //If user not found
+      if(!user){
+        throw new BadRequestException("User not found.");
+      }
+      //Check if the username already have or not
+      if(updateUserDto.username === user.username){
+        throw new ConflictException(`This username have been already used.`);
+      }
 
      //Check if the username already have or not
-     if(updateUserDto.email === user.email){
-      throw new Error(`This email have been already used.`);
-    }
+      if(updateUserDto.email === user.email){
+        throw new ConflictException(`This email have been already used.`);
+      }
 
-    return await this.prisma.users.update({
-      where: { username },
-      // Prisma will only update the fields that are actually inside this DTO
-      data: updateUserDto, 
-    });
+      return await this.prisma.users.update({
+        where: { username },
+        // Prisma will only update the fields that are actually inside this DTO
+        data: updateUserDto, 
+      });
+    }catch(error){
+      throw new InternalServerErrorException("Something went wrong at UpdateProfile endpoint at UserService.");
+    }
+  
+
+
+
+    
   }
 
   //User Update their own password
   async changePassword(username: string, dto: ChangePasswordDto){
-    //1.Find user first
-    const user = await this.prisma.users.findUnique( {where:{username}});
-    //If user not found
-    if(!user){
-      throw new BadRequestException("User not found.");
+    try{
+      //1.Find user first
+      const user = await this.prisma.users.findUnique( {where:{username}});
+      //If user not found
+      if(!user){
+        throw new BadRequestException("User not found.");
+      }
+  
+      //2.Verify old password first
+      const IsPasswordValid = await bcrypt.compare(dto.oldPassword, user.userPassword); 
+      // bcrypt.compare() - function securely verifies a user-provided plaintext password against a stored hashed password.
+      //if old password didnt match with the hash pasword version in database
+      if(!IsPasswordValid){
+        throw new BadRequestException("Old password is incorrect, please retry again.");
+      }
+  
+      //If password matched
+      //3.Hash new password
+      const hashedPassword = await bcrypt.hash(dto.newPassword, 12);
+  
+      return this.prisma.users.update({
+        where: {username},
+        data: {userPassword: hashedPassword}
+      });
+
+    }catch(error){
+      throw new InternalServerErrorException("Something went wrong at change-password endpoint at UserService.");
     }
-
-    //2.Verify old password first
-    const IsPasswordValid = await bcrypt.compare(dto.oldPassword, user.userPassword); 
-    // bcrypt.compare() - function securely verifies a user-provided plaintext password against a stored hashed password.
-    //if old password didnt match with the hash pasword version in database
-    if(!IsPasswordValid){
-      throw new BadRequestException("Old password is incorrect, please retry again.");
-    }
-
-    //If password matched
-    //3.Hash new password
-    const hashedPassword = await bcrypt.hash(dto.newPassword, 12);
-
-    return this.prisma.users.update({
-      where: {username},
-      data: {userPassword: hashedPassword}
-    });
   }
 
 
